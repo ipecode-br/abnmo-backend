3af53e35bdc93dbcacab46138d8220cee4f4ce59
Juliano Sill <sill.juliano@gmail.com>
2025-05-23 22:30:16 -0300

    build(lambda): test new lambda config

--- scripts/build-lambda.ts
+++ scripts/build-lambda.ts
@@ -1,7 +1,8 @@
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
+import path from 'node:path';
+
 import { build } from 'esbuild';
 import fs from 'fs-extra';
-import path from 'path';
 
 const outDir = 'dist-lambda';
 const zipFile = 'lambda.zip';
@@ -10,14 +11,11 @@ const envFile = '.env';
 async function bundleLambda() {
   console.log('🚀 Building Lambda bundle...');
 
-  // Clean directories
   await fs.remove(outDir);
   await fs.ensureDir(outDir);
 
-  // 1. First run NestJS build
   execSync('npm run build', { stdio: 'inherit' });
 
-  // 2. Use esbuild to bundle the lambda entry point
   await build({
     entryPoints: ['infra/lambda.ts'],
     bundle: true,
@@ -33,14 +31,11 @@ async function bundleLambda() {
     minify: true,
   });
 
-  // 3. Copy necessary files
   if (await fs.pathExists(envFile)) {
     await fs.copy(envFile, path.join(outDir, '.env'));
   }
   await fs.copy('dist', path.join(outDir, 'dist'));
-  await fs.copy('node_modules', path.join(outDir, 'node_modules'));
 
-  // 4. Create zip
   execSync(`cd ${outDir} && zip -r ../${zipFile} .`, { stdio: 'inherit' });
 
   console.log(`✅ Lambda bundle created: ${zipFile}`);
