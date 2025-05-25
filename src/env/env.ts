ea26d67d86e1ed665fc0fcc1f42054ef13c4fd16
Juliano Sill <sill.juliano@gmail.com>
2025-05-24 22:55:30 -0300

    build(lambda): create dev pipelien to deploy to aws

--- src/env/env.ts
+++ src/env/env.ts
@@ -12,7 +12,6 @@ export const envSchema = z.object({
   DB_HOST: z.string().min(1),
   DB_PORT: z.coerce.number(),
   DB_DATABASE: z.string().min(1),
-  DB_ROOT_PASSWORD: z.string().min(1),
   DB_USERNAME: z.string().min(1),
   DB_PASSWORD: z.string().min(1),
 });
