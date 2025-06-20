7c66d8ab5a06f68f8f5a92a3db4f8936d6603687
Juliano Sill <sill.juliano@gmail.com>
2025-06-19 23:08:14 -0300

    feat(utils): create utils module with cookies handler

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
+  APP_URL: z.string().url(),
+
   // API
+  API_BASE_URL: z.string().url(),
+  API_PORT: z.coerce.number().default(3333),
+  API_ENVIRONMENT: z
+    .enum(['production', 'development', 'homolog', 'local'])
+    .default('local'),
   NODE_ENV: z
     .enum(['production', 'development', 'test'])
     .default('development'),
-  API_BASE_URL: z.string().url(),
-  API_PORT: z.coerce.number().min(1),
+
+  // Secrets
+  COOKIE_DOMAIN: z.string().optional(),
+  COOKIE_SECRET: z.string().min(1),
   JWT_SECRET: z.string().min(1),
 
   // Database
