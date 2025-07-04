778c4ce1cd5977a373b1b3370f79dfa1c1448d53
jorgeCasalini <joshcasalini@gmail.com>
2025-07-04 18:06:33 -0300

    feat(patient-support): change entity to ZOD standard

--- src/domain/entities/patient-support.ts
+++ src/domain/entities/patient-support.ts
@@ -1,53 +1,42 @@
-// import { ApiProperty } from '@nestjs/swagger';
-// import {
-//   Column,
-//   Entity,
-//   JoinColumn,
-//   ManyToOne,
-//   PrimaryGeneratedColumn,
-// } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  //JoinColumn,
+  //ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 // import { Patient } from '@/domain/entities/patient';
+import { PatientSuppoprtSchema } from '../schemas/patient-support';
 
-// @Entity('support')
-// export class PatientSupport {
-//   @ApiProperty({
-//     example: 1,
-//     description: 'Identificador único do apoio',
-//   })
-//   @PrimaryGeneratedColumn()
-//   id_support: number;
+@Entity('support')
+export class PatientSupport implements PatientSuppoprtSchema {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-//   @ApiProperty({
-//     example: 'Amélia da Silva',
-//     description: 'Nome completo do apoio.',
-//     required: true,
-//   })
-//   @Column({ type: 'varchar', length: 100, nullable: false })
-//   support_name: string;
+  @Column({ type: 'varchar', length: 255 })
+  patient_id: string;
 
-//   @ApiProperty({
-//     example: 'Mãe',
-//     description: 'Grau de parentesco. Ex: Mãe, Irmão, Primo, etc.',
-//     required: true,
-//   })
-//   @Column({ type: 'varchar', length: 50, nullable: false })
-//   relation: string;
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
 
-//   @ApiProperty({
-//     example: '11987654321',
-//     description:
-//       'Número de Whatsapp do apoio, sem espaços ou caracteres especiais.',
-//     required: true,
-//   })
-//   @Column({ type: 'char', length: 11, nullable: false })
-//   whatsapp: string;
+  @Column({ type: 'char', length: 11 })
+  phone: string;
 
-//   @ManyToOne(() => Patient)
-//   @JoinColumn({ name: 'id_paciente' })
-//   patient: Patient;
+  @Column({ type: 'varchar', length: 50 })
+  kinship: string;
 
-//   @ApiProperty({ example: 1, description: 'Identificador único do paciente' })
-//   @Column({ type: 'integer' })
-//   id_patient: number;
-// }
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  /*
+  @ManyToOne(() => Patient)
+  @JoinColumn({ name: patient_id })
+  patient: Patient;
+  */
+}
