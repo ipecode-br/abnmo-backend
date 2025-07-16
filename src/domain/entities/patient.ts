2d48473a7f490f1510591b6c77acc44051f721d9
Juliano Sill <sill.juliano@gmail.com>
2025-07-15 23:00:02 -0300

    chore(entities): fix patients column name and user relation

--- src/domain/entities/patient.ts
+++ src/domain/entities/patient.ts
@@ -3,20 +3,18 @@ import {
   CreateDateColumn,
   Entity,
   JoinColumn,
-  // ManyToOne,
   // OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
-// import { Diagnostic } from '@/domain/entities/diagnostic';
 // import { PatientSupport } from '@/domain/entities/patient-support';
 import { User } from '@/domain/entities/user';
 
 import { GENDERS, GenderType, PatientSchema } from '../schemas/patient';
 
-@Entity('pacientes')
+@Entity('patients')
 export class Patient implements PatientSchema {
   @PrimaryGeneratedColumn('uuid')
   id: string;
@@ -42,9 +40,6 @@ export class Patient implements PatientSchema {
   @Column({ type: 'varchar', length: 50, nullable: false })
   city: string;
 
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  url_photo: string | null;
-
   @Column({ type: 'tinyint', width: 1, default: 0, nullable: false })
   has_disability: boolean;
 
@@ -60,33 +55,18 @@ export class Patient implements PatientSchema {
   @Column({ type: 'varchar', length: 500, nullable: true })
   medication_desc: string | null;
 
-  @Column({ type: 'varchar', length: 200, nullable: true })
-  filename_diagnostic: string | null;
-
   @Column({ type: 'tinyint', width: 1, default: 0 })
   has_nmo_diagnosis: boolean;
 
   @CreateDateColumn({ type: 'timestamp' })
   created_at: Date;
 
-  @UpdateDateColumn({
-    type: 'timestamp',
-  })
+  @UpdateDateColumn({ type: 'timestamp' })
   updated_at: Date;
 
-  @Column({ type: 'tinyint', width: 1, default: 0 })
-  flag_active: boolean;
-
   @OneToOne(() => User)
-  @JoinColumn({ name: 'id_usuario' })
+  @JoinColumn({ name: 'user_id' })
   user: User;
-
-  // @Column({ name: 'id_diagnostic', type: 'int', nullable: false })
-  // id_diagnostic: number;
-
-  // @ManyToOne(() => Diagnostic, { nullable: false })
-  // @JoinColumn({ name: 'id_diagnostic' })
-  // diagnostic: Diagnostic;
 
   // @OneToMany(() => PatientSupport, (support) => support.patient)
   // support: PatientSupport[];
