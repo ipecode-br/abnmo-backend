278041131613b2b55cf915010234eea156c969df
Juliano Sill <sill.juliano@gmail.com>
2025-07-20 16:01:31 -0300

    feat(guards): create `RolesGuard` to validate permissions

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,53 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import type { UserSchema } from '@/domain/schemas/user';
+
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { Roles } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<{ user?: UserSchema }>();
+
+    const user = request.user;
+
+    if (!user) {
+      throw new UnauthorizedException(
+        'Você não tem permissão para executar esta ação.',
+      );
+    }
+
+    const roles = this.reflector.getAllAndMerge(Roles, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const isAllowed = roles.includes(user.role);
+
+    if (!isAllowed) {
+      throw new UnauthorizedException(
+        'Você não tem permissão para executar esta ação.',
+      );
+    }
+
+    return true;
+  }
+}
