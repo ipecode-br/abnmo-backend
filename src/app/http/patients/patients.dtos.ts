e14ef9f3c30f2a5dbabd0b82548df706ea2a2159
Jefferson Nunes <96424928+jeffynunes09@users.noreply.github.com>
2025-07-24 19:05:09 -0300

    Update patients.dtos.ts

--- src/app/http/patients/patients.dtos.ts
+++ src/app/http/patients/patients.dtos.ts
@@ -1,9 +1,7 @@
 import { createZodDto } from 'nestjs-zod';
-
 import {
   createPatientSchema,
   findAllPatientSchema,
 } from '@/domain/schemas/patient';
-
 export class CreatePatientDto extends createZodDto(createPatientSchema) {}
 export class FindAllPatientDto extends createZodDto(findAllPatientSchema) {}
