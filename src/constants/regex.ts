443dd13db56e70652eee6259088fc0592f0dc01a
Juliano Sill <sill.juliano@gmail.com>
2025-07-28 21:43:14 -0300

    feat(constants): create `regex.ts` file with base regexes

---
+++ src/constants/regex.ts
@@ -0,0 +1,19 @@
+export const NON_NUMBER_REGEX = /\D/g;
+export const NUMBER_REGEX = /\d/;
+
+export const NAME_REGEX =
+  /^(?!\s)(?!.*\s{2})[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+){1,49}$/;
+export const UPPERCASE_REGEX = /^(?=.*[A-Z]).*$/;
+export const LOWERCASE_REGEX = /^(?=.*[a-z]).*$/;
+export const SPECIAL_CHAR_REGEX = /^(?=.*\W).*$/;
+export const NON_SPECIAL_CHAR_REGEX = /^[a-zA-ZÀ-ÿ\s'-]{1,50}$/;
+
+/*
+  (?=.*\d)    should contain at least 1 digit
+  (?=.*[a-z]) should contain at least 1 lowercase letter
+  (?=.*[A-Z]) should contain at least 1 uppercase letter
+  (?=.*\W)    should contain at least 1 special character
+  (?!.*\s)    should not contain any blank space
+*/
+export const PASSWORD_REGEX =
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).*$/;
