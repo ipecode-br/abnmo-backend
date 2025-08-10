ef2e4b833e6e96ff8a672adeef46b7961bfae6d8
Juliano Sill <sill.juliano@gmail.com>
2025-08-09 23:10:48 -0300

    chore(infra): update `seed-dev` script

--- infra/database/seed-dev.ts
+++ infra/database/seed-dev.ts
@@ -1,85 +1,82 @@
-import 'reflect-metadata';
-
 import { faker } from '@faker-js/faker';
+import { hash } from 'bcryptjs';
+
+import { BRAZILIAN_STATES } from '@/constants/brazilian-states';
+import { GENDERS, PATIENT_STATUS } from '@/domain/schemas/patient';
+import { USER_ROLES } from '@/domain/schemas/user';
 
 import { Patient } from '../../src/domain/entities/patient';
 import { PatientSupport } from '../../src/domain/entities/patient-support';
 import { User } from '../../src/domain/entities/user';
 import dataSource from './data.source';
 
-const DATABASE_DEV_NAME = 'abnmo_db';
+const DATABASE_DEV_NAME = 'abnmo_dev';
+
+async function hashPassword(password: string) {
+  return hash(password, 10);
+}
 
 async function main() {
+  const password = await hashPassword('12345678');
+
   try {
     await dataSource.initialize();
     const dbName = dataSource.options.database;
 
     if (dbName !== DATABASE_DEV_NAME) {
       console.log(
-        `❌ Execução negada. O banco atual não é o de desenvolvimento (${DATABASE_DEV_NAME})`,
+        '❌ Permission denied. The current database is not the development one.',
       );
       process.exit(1);
     }
 
-    console.log('🧹 Limpando banco de dados...');
+    console.log('📦 Running migrations...');
+    await dataSource.runMigrations();
+    console.log('✅ Migrations completed.');
+
+    console.log('🧹 Cleaning database...');
     await dataSource.query('SET FOREIGN_KEY_CHECKS = 0');
     await dataSource.manager.clear(PatientSupport);
     await dataSource.manager.clear(Patient);
     await dataSource.manager.clear(User);
     await dataSource.query('SET FOREIGN_KEY_CHECKS = 1');
-
-    console.log('✅ Dados antigos apagados.');
-
-    console.log('📦 Executando migrations...');
-    await dataSource.runMigrations();
-    console.log('✅ Migrations executadas.');
+    console.log('✅ Old data deleted.');
 
     const userRepository = dataSource.getRepository(User);
     const patientRepository = dataSource.getRepository(Patient);
-    const supportRepository = dataSource.getRepository(PatientSupport);
+    const supportNetworkRepository = dataSource.getRepository(PatientSupport);
 
-    const roles = ['admin', 'nurse', 'specialist', 'manager'] as const;
-
-    console.log('👤 Criando usuários de sistema...');
-    for (const role of roles) {
+    console.log('👤 Creating users...');
+    for (const role of USER_ROLES) {
       const user = userRepository.create({
         name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
+        email: `${role}@ipecode.com.br`,
+        password,
         role,
         avatar_url: faker.image.avatar(),
       });
-
       await userRepository.save(user);
-      console.log(`✅ Usuário ${role} criado: ${user.email}`);
     }
+    console.log('👤 Users created successfully...');
 
-    console.log('🧑‍⚕️ Criando 50 pacientes...');
+    console.log('👤 Creating 50 patients...');
     for (let i = 0; i < 50; i++) {
       const user = userRepository.create({
         name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
+        email: faker.internet.email().toLocaleLowerCase(),
+        password,
         role: 'patient',
         avatar_url: faker.image.avatar(),
       });
-
       await userRepository.save(user);
 
       const patient = patientRepository.create({
-        user,
-        gender: faker.helpers.arrayElement([
-          'male_cis',
-          'female_cis',
-          'male_trans',
-          'female_trans',
-          'non_binary',
-          'prefer_not_to_say',
-        ]),
+        user_id: user.id,
+        gender: faker.helpers.arrayElement(GENDERS),
         date_of_birth: faker.date.birthdate({ min: 18, max: 80, mode: 'age' }),
         phone: faker.string.numeric(11),
         cpf: faker.string.numeric(11),
-        state: 'PE',
+        state: faker.helpers.arrayElement(BRAZILIAN_STATES),
         city: faker.location.city(),
         has_disability: faker.datatype.boolean(),
         disability_desc: faker.lorem.sentence(),
@@ -87,37 +84,32 @@ async function main() {
         take_medication: faker.datatype.boolean(),
         medication_desc: faker.lorem.sentence(),
         has_nmo_diagnosis: faker.datatype.boolean(),
-        status: 'active',
+        status: faker.helpers.arrayElement(PATIENT_STATUS),
       });
-
       await patientRepository.save(patient);
 
-      const supportCount = faker.number.int({ min: 0, max: 3 });
-      for (let j = 0; j < supportCount; j++) {
-        const support = supportRepository.create({
+      const supportNetworkCount = faker.number.int({ min: 0, max: 3 });
+      for (let j = 0; j < supportNetworkCount; j++) {
+        const support = supportNetworkRepository.create({
           patient: patient,
           name: faker.person.fullName(),
           phone: faker.string.numeric(11),
           kinship: faker.helpers.arrayElement([
-            'pai',
-            'mãe',
-            'irmão',
-            'cônjuge',
+            'Pai',
+            'Mãe',
+            'Irmão',
+            'Filho',
+            'Avó',
           ]),
         });
-
-        await supportRepository.save(support);
+        await supportNetworkRepository.save(support);
       }
-
-      console.log(
-        `✅ Paciente ${i + 1} criado com ${supportCount} contato(s) de apoio`,
-      );
     }
 
-    console.log('🎉 Seed finalizado com sucesso.');
+    console.log('🎉 Seed completed successfully.');
     process.exit(0);
   } catch (err) {
-    console.error('❌ Erro ao executar seed:', err);
+    console.error('❌ Error executing seed:', err);
     process.exit(1);
   }
 }
