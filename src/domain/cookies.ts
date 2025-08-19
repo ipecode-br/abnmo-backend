cef186e9ea319b3c8c9427549dc85b65d5503de7
Hugo França <hugo.francaa06@gmail.com>
2025-08-18 22:01:46 -0300

    chore(cookies): update cookie type

--- src/domain/cookies.ts
+++ src/domain/cookies.ts
@@ -1,9 +1,9 @@
 import type { AuthTokenType } from './schemas/token';
 
 export type Cookie = AuthTokenType;
-export type Cookies = Record<Cookie, string>;
+export type Cookies = Record<Cookie, Cookie>;
 
 export const COOKIES_MAPPER: Cookies = {
   access_token: 'access_token',
   password_reset: 'password_reset',
-};
+} as const;
