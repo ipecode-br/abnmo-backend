ee535f90aeb04233e35a15cfa91a8dbb0d860ced
Hugo França <hugo.francaa06@gmail.com>
2025-08-28 15:36:10 -0300

    refactor(appointment): create endpoint cancel appontiment

--- src/app/http/appointments/appointments.controller.ts
+++ src/app/http/appointments/appointments.controller.ts
@@ -1,5 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Patch } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+
+import { CurrentUser } from '@/common/decorators/current-user.decorator';
+import { Roles } from '@/common/decorators/roles.decorator';
+import { CancelAppointmentResponseSchema } from '@/domain/schemas/appointment';
+import { UserSchema } from '@/domain/schemas/user';
 
 import { AppointmentsService } from './appointments.service';
 
@@ -7,4 +12,18 @@ import { AppointmentsService } from './appointments.service';
 @Controller('appointments')
 export class AppointmentsController {
   constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @Roles(['nurse', 'manager', 'specialist'])
+  @Patch(':id/cancel')
+  async cancel(
+    @Param('id') id: string,
+    @CurrentUser() user: UserSchema,
+  ): Promise<CancelAppointmentResponseSchema> {
+    await this.appointmentsService.cancelAppointment(id, user);
+
+    return {
+      success: true,
+      message: 'Atendimento cancelado com sucesso.',
+    };
+  }
 }
--- src/app/http/appointments/appointments.service.ts
+++ src/app/http/appointments/appointments.service.ts
@@ -1,4 +1,12 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { UserSchema } from '@/domain/schemas/user';
 
 import { AppointmentsRepository } from './appointments.repository';
 
@@ -9,4 +17,31 @@ export class AppointmentsService {
   constructor(
     private readonly appointmentsRepository: AppointmentsRepository,
   ) {}
+
+  async cancelAppointment(id: string, user: UserSchema): Promise<void> {
+    const appointment = await this.appointmentsRepository.findById(id);
+
+    if (!appointment) {
+      throw new NotFoundException('Atendimento não encontrado.');
+    }
+
+    if (!['nurse', 'manager', 'specialist'].includes(user.role)) {
+      throw new ForbiddenException(
+        'Você não tem permissão para cancelar atendimento.',
+      );
+    }
+
+    if (appointment.status === 'canceled') {
+      throw new BadRequestException('Este atendimento já está cancelado.');
+    }
+
+    appointment.status = 'canceled';
+
+    await this.appointmentsRepository.save(appointment);
+
+    this.logger.log(
+      { id: appointment.id, user: user.id },
+      'Atendimento cancelado com sucesso.',
+    );
+  }
 }
