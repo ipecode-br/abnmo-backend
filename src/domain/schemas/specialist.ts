da612561792a3e83f6c0ab184fffe63172534fc4
Hugo França <hugo.francaa06@gmail.com>
2025-09-22 17:03:50 -0300

    feat(specialists): implement create zod schema

--- src/domain/schemas/specialist.ts
+++ src/domain/schemas/specialist.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+import { baseResponseSchema } from './base';
+
 export const SPECIALIST_STATUS = ['active', 'inactive'] as const;
 export type SpecialistStatusType = (typeof SPECIALIST_STATUS)[number];
 
@@ -14,3 +16,38 @@ export const specialistSchema = z
   })
   .strict();
 export type SpecialistSchema = z.infer<typeof specialistSchema>;
+
+//create
+
+export const createSpecialistSchema = specialistSchema
+  .omit({ id: true, created_at: true, updated_at: true })
+  .extend({
+    user_id: z.string().uuid().optional(),
+    name: z.string().optional(),
+    email: z.string().email().optional(),
+  })
+  .refine(
+    (data) => {
+      const hasNameAndEmail = !!data.name && !!data.email;
+      if (!data.user_id && !hasNameAndEmail) {
+        return false;
+      }
+      if (data.user_id) {
+        data.name = undefined;
+        data.email = undefined;
+      }
+      return true;
+    },
+    {
+      message:
+        'Fields `name` and `email` are required when `user_id` is not provided.',
+      path: ['root'],
+    },
+  );
+
+export type CreateSpecialistSchema = z.infer<typeof createSpecialistSchema>;
+
+export const createSpecialistResponseSchema = baseResponseSchema.extend({});
+export type CreateSpecialistResponseSchema = z.infer<
+  typeof createSpecialistResponseSchema
+>;
