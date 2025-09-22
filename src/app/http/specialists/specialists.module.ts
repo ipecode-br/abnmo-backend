34c6b1b8da41c9eed5a51f32c279bf838006ea64
Hugo França <hugo.francaa06@gmail.com>
2025-09-22 17:06:12 -0300

    feat(specialists): specialists module adjustment

--- src/app/http/specialists/specialists.module.ts
+++ src/app/http/specialists/specialists.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CryptographyModule } from '@/app/cryptography/cryptography.module';
 import { Specialist } from '@/domain/entities/specialist';
 
+import { UsersModule } from '../users/users.module';
 import { SpecialistsController } from './specialists.controller';
 import { SpecialistsRepository } from './specialists.repository';
 import { SpecialistsService } from './specialists.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([Specialist])],
+  imports: [
+    CryptographyModule,
+    UsersModule,
+    TypeOrmModule.forFeature([Specialist]),
+  ],
   controllers: [SpecialistsController],
   providers: [SpecialistsService, SpecialistsRepository],
   exports: [SpecialistsRepository],
