3b25e7ee48798cc6828347ffedb41ba319137f1a
jorgeCasalini <joshcasalini@gmail.com>
2025-09-25 16:16:03 -0300

    feat(cookies): add invite_token to cookies mapper

--- src/domain/cookies.ts
+++ src/domain/cookies.ts
@@ -6,4 +6,5 @@ export type Cookies = Record<Cookie, Cookie>;
 export const COOKIES_MAPPER: Cookies = {
   access_token: 'access_token',
   password_reset: 'password_reset',
+  invite_token: 'invite_token',
 } as const;
