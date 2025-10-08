fec8251633b18f009e8021648945f0f39cf8e78c
Paulo Roberto <paulorantunes13@hotmail.com>
2025-10-08 17:26:47 -0300

    feat(specialists): implement validation schemas for findAll endpoint

--- src/domain/schemas/specialist.ts
+++ src/domain/schemas/specialist.ts
@@ -1,10 +1,21 @@
 import { z } from 'zod';
 
 import { appointmentSchema } from './appointment';
+import { baseResponseSchema } from './base';
+import { baseQuerySchema } from './query';
 import { userSchema } from './user';
 
 export const SPECIALIST_STATUS = ['active', 'inactive', 'pending'] as const;
 export type SpecialistStatusType = (typeof SPECIALIST_STATUS)[number];
+
+export const SPECIALIST_ORDER_BY = [
+  'name',
+  'specialty',
+  'registry',
+  'status',
+  'date',
+] as const;
+export type SpecialistOrderByType = (typeof SPECIALIST_ORDER_BY)[number];
 
 export const INVITE_ROLES = ['specialist', 'manager', 'nurse'] as const;
 export type InviteRolesType = (typeof INVITE_ROLES)[number];
@@ -36,3 +47,42 @@ export const createInviteSchema = userSchema
   .pick({ email: true })
   .merge(z.object({ type: z.enum(INVITE_ROLES) }));
 export type CreateInviteDto = z.infer<typeof createInviteSchema>;
+
+export const findAllSpecialistQuerySchema = baseQuerySchema
+  .pick({
+    search: true,
+    order: true,
+    page: true,
+    perPage: true,
+    startDate: true,
+    endDate: true,
+  })
+  .extend({
+    status: z.enum(SPECIALIST_STATUS).optional(),
+    orderBy: z.enum(SPECIALIST_ORDER_BY).optional().default('name'),
+  })
+  .refine(
+    (data) => {
+      if (data.startDate && data.endDate) {
+        return data.startDate < data.endDate;
+      }
+      return true;
+    },
+    {
+      message: 'It should be greater than `startDate`',
+      path: ['endDate'],
+    },
+  );
+export type FindAllSpecialistQuerySchema = z.infer<
+  typeof findAllSpecialistQuerySchema
+>;
+
+export const findAllSpecialistsResponseSchema = baseResponseSchema.extend({
+  data: z.object({
+    specialists: z.array(specialistResponseSchema),
+    total: z.number(),
+  }),
+});
+export type FindAllSpecialistsResponseSchema = z.infer<
+  typeof findAllSpecialistsResponseSchema
+>;
