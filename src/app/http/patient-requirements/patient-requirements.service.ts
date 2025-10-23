eb1bc8510b1a9808bd91538308a8a68ad2a1c9c0
Hugo França <hugo.francaa06@gmail.com>
2025-10-23 12:41:46 -0300

    feat(patients-requirements): create function to aprove patient requirement service

--- src/app/http/patient-requirements/patient-requirements.service.ts
+++ src/app/http/patient-requirements/patient-requirements.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { UserSchema } from '@/domain/schemas/user';
+
+import { PatientsRequirementRepository } from './patient-requirements.repository';
 
 @Injectable()
-export class PatientRequirementsService {}
+export class PatientRequirementsService {
+  private readonly logger = new Logger(PatientRequirementsService.name);
+
+  constructor(
+    private readonly patientsRequirementRepository: PatientsRequirementRepository,
+  ) {}
+
+  async approveRequirement(id: string, user: UserSchema): Promise<void> {
+    const patientRequirement =
+      await this.patientsRequirementRepository.findById(id);
+
+    if (!patientRequirement) {
+      throw new NotFoundException(`Solicitação de id ${id} não encontrada`);
+    }
+
+    if (patientRequirement.status != 'under_review') {
+      throw new ConflictException(
+        'Solicitação precisa ter status under_review para ser aprovada',
+      );
+    }
+
+    await this.patientsRequirementRepository.approvedRequirement(id, user.id);
+
+    this.logger.log(
+      {
+        id: patientRequirement.id,
+        userId: user.id,
+        approvedAt: new Date(),
+      },
+      'Requirement approved successfully',
+    );
+  }
+}
