88885cdaeded7add89d29cee58f48de583eb30ab
jorgeCasalini <joshcasalini@gmail.com>
2025-10-31 17:42:50 -0300

    feat(patient): add 'pending' status to patient schema

--- src/domain/schemas/patient.ts
+++ src/domain/schemas/patient.ts
@@ -21,7 +21,7 @@ export const GENDERS = [
 ] as const;
 export type GenderType = (typeof GENDERS)[number];
 
-export const PATIENT_STATUS = ['active', 'inactive'] as const;
+export const PATIENT_STATUS = ['active', 'inactive', 'pending'] as const;
 export type PatientStatusType = (typeof PATIENT_STATUS)[number];
 
 export const PATIENT_ORDER_BY = ['name', 'email', 'status', 'date'] as const;
