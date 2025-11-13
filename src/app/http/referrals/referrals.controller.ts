64d6f43876a9ad7ab111b50267191b4852c1302d
Hugo França <hugo.francaa06@gmail.com>
2025-11-13 10:20:06 -0300

    feat(referrals): set up module structure with service and controller

---
+++ src/app/http/referrals/referrals.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { ReferralsService } from './referrals.service';
+
+@Controller('referrals')
+export class ReferralsController {
+  constructor(private readonly referralsService: ReferralsService) {}
+}
