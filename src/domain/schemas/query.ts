5899f854488193fca83cbb102ac2e140389241aa
Paulo Roberto <paulorantunes13@hotmail.com>
2025-12-04 00:35:01 -0300

    feat(query): change default period from 'last-week' to 'today'

--- src/domain/schemas/query.ts
+++ src/domain/schemas/query.ts
@@ -3,13 +3,18 @@ import { z } from 'zod';
 export const ORDER = ['ASC', 'DESC'] as const;
 export type OrderType = (typeof ORDER)[number];
 
-export const PERIOD = ['last-year', 'last-month', 'last-week'] as const;
+export const PERIOD = [
+  'last-year',
+  'last-month',
+  'last-week',
+  'today',
+] as const;
 export type PeriodType = (typeof PERIOD)[number];
 
 export const baseQuerySchema = z.object({
   search: z.string().optional(),
   order: z.enum(ORDER).optional(),
-  period: z.enum(PERIOD).optional().default('last-week'),
+  period: z.enum(PERIOD).optional().default('today'),
   page: z.coerce.number().min(1).optional().default(1),
   perPage: z.coerce.number().min(1).max(50).optional().default(10),
   limit: z.coerce.number().min(1).optional().default(10),
