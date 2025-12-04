fb1ef7415a2099263945034221b1415125fe8754
Paulo Roberto <paulorantunes13@hotmail.com>
2025-12-04 00:46:54 -0300

    chore(referrals): update ReferralsModule dependencies

--- src/app/http/referrals/referrals.module.ts
+++ src/app/http/referrals/referrals.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Referral } from '@/domain/entities/referral';
+
+import { PatientsModule } from '../patients/patients.module';
 import { ReferralsController } from './referrals.controller';
+import { ReferralsRepository } from './referrals.repository';
 import { ReferralsService } from './referrals.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Referral]), PatientsModule],
   controllers: [ReferralsController],
-  providers: [ReferralsService],
+  providers: [ReferralsService, ReferralsRepository],
+  exports: [ReferralsRepository, ReferralsService],
 })
 export class ReferralsModule {}
