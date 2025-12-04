c90b5d82488873e532c59e018adfbe85fed953ae
Paulo Roberto <paulorantunes13@hotmail.com>
2025-12-04 00:48:53 -0300

    chore(statistics): add ReferralsModule imports

--- src/app/http/statistics/statistics.module.ts
+++ src/app/http/statistics/statistics.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { UtilsModule } from '@/utils/utils.module';
 
 import { PatientsModule } from '../patients/patients.module';
+import { ReferralsModule } from '../referrals/referrals.module';
 import { StatisticsController } from './statistics.controller';
 import { StatisticsService } from './statistics.service';
 
 @Module({
-  imports: [PatientsModule, UtilsModule],
+  imports: [PatientsModule, UtilsModule, ReferralsModule],
   controllers: [StatisticsController],
   providers: [StatisticsService],
 })
