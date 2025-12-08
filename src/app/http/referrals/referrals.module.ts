af3b1f9deba416c462f33d913bbfc88d28cecbeb
Juliano Sill <sill.juliano@gmail.com>
2025-12-08 00:18:12 -0300

    chore(referrals): remove unused repository file

--- src/app/http/referrals/referrals.module.ts
+++ src/app/http/referrals/referrals.module.ts
@@ -5,7 +5,6 @@ import { Patient } from '@/domain/entities/patient';
 import { Referral } from '@/domain/entities/referral';
 
 import { ReferralsController } from './referrals.controller';
-import { ReferralsRepository } from './referrals.repository';
 import { CancelReferralUseCase } from './use-cases/cancel-referral.use-case';
 import { CreateReferralUseCase } from './use-cases/create-referrals.use-case';
 import { GetReferralsUseCase } from './use-cases/get-referrals.use-case';
@@ -17,11 +16,9 @@ import { GetReferralsUseCase } from './use-cases/get-referrals.use-case';
   ],
   controllers: [ReferralsController],
   providers: [
-    ReferralsRepository,
     GetReferralsUseCase,
     CreateReferralUseCase,
     CancelReferralUseCase,
   ],
-  exports: [ReferralsRepository],
 })
 export class ReferralsModule {}
