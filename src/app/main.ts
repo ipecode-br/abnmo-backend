6ec09d4c63a81f815590838ff2e9e240fe06124a
Juliano Sill <sill.juliano@gmail.com>
2026-05-19 10:42:41 -0300

    chore: move swagger document from `app.ts` to `main.ts`

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,38 @@
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
+
 import { EnvService } from '@/env/env.service';
 
 import { createNestApp } from './app';
 
 async function bootstrap(): Promise<void> {
   const app = await createNestApp();
+
+  const config = new DocumentBuilder()
+    .setTitle('SVM - Sistema Viver Melhor')
+    .setDescription(
+      'Esta documentação lista as rotas disponíveis da aplicação, bem como seus respectivos requisitos e dados retornados.',
+    )
+    .setVersion('0.0.1')
+    .build();
+
+  const cleanupSwagger = cleanupOpenApiDoc as unknown as (
+    doc: OpenAPIObject,
+  ) => OpenAPIObject;
+
+  const swaggerDocument = SwaggerModule.createDocument(app, config);
+  const cleanedDocument = cleanupSwagger(swaggerDocument);
+
+  SwaggerModule.setup('/swagger', app, cleanedDocument, {
+    swaggerOptions: {
+      withCredentials: true,
+      persistAuthorization: true,
+      requestInterceptor: (request: { credentials?: string }) => {
+        request.credentials = 'include';
+        return request;
+      },
+    },
+  });
 
   const envService = app.get(EnvService);
 
